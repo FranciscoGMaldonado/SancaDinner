c0ff746cac9ed69e0bbe544eb33b53f5a9ade6cd
abelBaes <abel.baes77@gmail.com>
2026-03-24 21:56:01 -0300

    feat(ProductInventory): adding methods and atributes to ProductInventoryClass

--- src/main/java/com/ifsp/edu/sanca_dinner/domain/model/ProductInventory.java
+++ src/main/java/com/ifsp/edu/sanca_dinner/domain/model/ProductInventory.java
@@ -10,10 +10,21 @@ public class ProductInventory {
     private Integer quantity;
 
     public ProductInventory(Product product, Integer quantity) {
+        validateProduct(product);
         this.product = product;
+        setQuantity(quantity);
+    }
+
+    private void validateProduct(Product product){
+        if(product == null) throw new DomainException("O produto não pode ser nulo.");
     }
 
     private void validateQuantity(Integer quantity){
         if(quantity == null || quantity < 0) throw new DomainException("A quantidade não pode ser negativa ou nula.");
+    }
+
+    public void setQuantity(Integer quantity) {
+        validateQuantity(quantity);
+        this.quantity = quantity;
     }
 }
