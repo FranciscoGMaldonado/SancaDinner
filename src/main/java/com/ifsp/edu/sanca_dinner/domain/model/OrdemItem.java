a5aecb316d5dce6f6b1d5670a303f5471efb946e
abelBaes <abel.baes77@gmail.com>
2026-03-27 20:11:19 -0300

    feat(OrderItem): adding OrderItem Class with constructor method.

---
+++ src/main/java/com/ifsp/edu/sanca_dinner/domain/model/OrdemItem.java
@@ -0,0 +1,39 @@
+package com.ifsp.edu.sanca_dinner.domain.model;
+
+import com.ifsp.edu.sanca_dinner.domain.exception.DomainException;
+import lombok.Getter;
+
+import java.util.UUID;
+
+@Getter
+public class OrdemItem {
+    private UUID id;
+    private Product product;
+    private String specification;
+    private OrderStatus orderStatus;
+
+    public OrdemItem(UUID id, Product product, OrderStatus orderStatus, String specification) {
+        setProduct(product);
+        setSpecification(specification);
+        this.orderStatus = OrderStatus.PENDING;
+        this.id = id;
+    }
+
+    void validateProduct(Product product){
+        if(product == null) throw new DomainException("O produto do item não pode ser nulo.");
+    }
+
+    void validateSpecification(String specification){
+        if(specification == null || specification.isBlank()) throw new DomainException("A especificação não pode ser vazia ou nula.");
+    }
+
+    public void setProduct(Product product) {
+        validateProduct(product);
+        this.product = product;
+    }
+
+    public void setSpecification(String specification) {
+        validateSpecification(specification);
+        this.specification = specification;
+    }
+}
---
+++ src/main/java/com/ifsp/edu/sanca_dinner/domain/model/OrderStatus.java
@@ -0,0 +1,8 @@
+package com.ifsp.edu.sanca_dinner.domain.model;
+
+public enum OrderStatus {
+    PENDING,
+    FINISHED,
+    DELIVERED,
+    CANCELED
+}
