84690bf87751457e44e63ce0bf5ba3859070b11c
abelBaes <abel.baes77@gmail.com>
2026-04-19 18:21:04 -0300

    fix(ProductMapper): adding component notation

--- src/main/java/com/ifsp/edu/sanca_dinner/controller/product/ProductMapper.java
+++ src/main/java/com/ifsp/edu/sanca_dinner/controller/product/ProductMapper.java
@@ -3,8 +3,10 @@ package com.ifsp.edu.sanca_dinner.controller.product;
 import com.ifsp.edu.sanca_dinner.controller.product.response.ProductResponse;
 import com.ifsp.edu.sanca_dinner.domain.model.product.Product;
 import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Component;
 
 @NoArgsConstructor
+@Component
 public class ProductMapper {
 
     public ProductResponse productToResponse(Product product){
