934b17a962518d960b4ec35c47de81104249f2c8
FranciscoGMaldonado <francisco.guaturamaldonado83@gmail.com>
2026-04-19 20:32:26 -0300

    feat(Auth): creation of AuthService, Controller and RegisterResponse

---
+++ src/main/java/com/ifsp/edu/sanca_dinner/controller/user/AuthController.java
@@ -0,0 +1,28 @@
+package com.ifsp.edu.sanca_dinner.controller.user;
+
+import com.ifsp.edu.sanca_dinner.controller.user.response.AuthResponse;
+import com.ifsp.edu.sanca_dinner.controller.user.request.LoginRequest;
+import com.ifsp.edu.sanca_dinner.controller.user.request.RegisterRequest;
+import com.ifsp.edu.sanca_dinner.controller.user.response.RegisterResponse;
+import com.ifsp.edu.sanca_dinner.domain.service.AuthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/register")
+    public ResponseEntity<RegisterResponse> register(@RequestBody RegisterRequest request) {
+        return ResponseEntity.ok(authService.register(request));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponse> login(@RequestBody LoginRequest request) {
+        return ResponseEntity.ok(authService.login(request));
+    }
+}
