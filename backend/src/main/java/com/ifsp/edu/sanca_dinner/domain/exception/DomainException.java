41108f34fd9f73b4e5c711fb7448535004fdc175
FranciscoGMaldonado <francisco.guaturamaldonado83@gmail.com>
2026-04-25 01:01:31 -0300

    chore(backend): moving backend to its own directory

---
+++ backend/src/main/java/com/ifsp/edu/sanca_dinner/domain/exception/DomainException.java
@@ -0,0 +1,7 @@
+package com.ifsp.edu.sanca_dinner.domain.exception;
+
+public class DomainException extends RuntimeException {
+    public DomainException(String message) {
+        super(message);
+    }
+}
--- src/main/java/com/ifsp/edu/sanca_dinner/domain/exception/DomainException.java
+++
@@ -1,7 +0,0 @@
-package com.ifsp.edu.sanca_dinner.domain.exception;
-
-public class DomainException extends RuntimeException {
-    public DomainException(String message) {
-        super(message);
-    }
-}
