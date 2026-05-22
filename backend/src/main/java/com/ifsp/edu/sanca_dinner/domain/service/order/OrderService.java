174baef5f0930652de004eadb51e54fae499aa96
abelBaes <abel.baes77@gmail.com>
2026-05-21 22:41:39 -0300

    feat(OrderService): adding changeOrderItem method

--- backend/src/main/java/com/ifsp/edu/sanca_dinner/domain/service/order/OrderService.java
+++ backend/src/main/java/com/ifsp/edu/sanca_dinner/domain/service/order/OrderService.java
@@ -3,6 +3,7 @@ package com.ifsp.edu.sanca_dinner.domain.service.order;
 import com.ifsp.edu.sanca_dinner.domain.exception.DomainException;
 import com.ifsp.edu.sanca_dinner.domain.model.order.Order;
 import com.ifsp.edu.sanca_dinner.domain.model.order_item.OrderItem;
+import com.ifsp.edu.sanca_dinner.domain.model.order_item.OrderItemStatus;
 import com.ifsp.edu.sanca_dinner.domain.repository.order.OrderRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -27,6 +28,17 @@ public class OrderService {
     public Order addOrderItem(Integer orderId, Integer productId, String specification, BigDecimal productPrice){
         var order = findOrderByIdWrapper(orderId);
         order.addOrderItem(new OrderItem(productId, specification, productPrice));
+        return orderRepository.save(order);
+    }
+
+    public Order changeOrderItem(Integer orderId, Integer orderItemId, String newSpecification){
+        var order = findOrderByIdWrapper(orderId);
+        var orderItem = order.getOrderItems().stream().
+                filter(item -> item.getId().equals(orderItemId)).
+                findFirst().
+                orElseThrow(() -> new DomainException("Item da comanda não encontrado."));
+        if(orderItem.getOrderItemStatus() != OrderItemStatus.PENDING) throw new DomainException("Apenas é possivel alterar itens que estão pendentes.");
+        orderItem.setSpecification(newSpecification);
         return orderRepository.save(order);
     }
 }
