5dd71384ed063d4728bf9e03cd3d61b9b2037dee
abelBaes <abel.baes77@gmail.com>
2026-05-21 22:21:45 -0300

    feat(OrderService): creating addOrderItem method

--- backend/src/main/java/com/ifsp/edu/sanca_dinner/domain/service/order/OrderService.java
+++ backend/src/main/java/com/ifsp/edu/sanca_dinner/domain/service/order/OrderService.java
@@ -1,9 +1,13 @@
 package com.ifsp.edu.sanca_dinner.domain.service.order;
 
+import com.ifsp.edu.sanca_dinner.domain.exception.DomainException;
 import com.ifsp.edu.sanca_dinner.domain.model.order.Order;
+import com.ifsp.edu.sanca_dinner.domain.model.order_item.OrderItem;
 import com.ifsp.edu.sanca_dinner.domain.repository.order.OrderRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
 
 @Service
 @AllArgsConstructor
@@ -14,5 +18,11 @@ public class OrderService {
     public Order createOrder(String customerName, Integer tableNumber){
         var newOrder = new Order(customerName, tableNumber);
         return orderRepository.save(newOrder);
+    }
+
+    public Order addOrderItem(Integer orderId, Integer productId, String specification, BigDecimal productPrice){
+        var order = orderRepository.findById(orderId).orElseThrow(() -> new DomainException("Comanda não encontrada."));
+        order.addOrderItem(new OrderItem(productId, specification, productPrice));
+        return orderRepository.save(order);
     }
 }
