8397c82ee925a3836505a4f0d9dda40c710aea51
abelBaes <abel.baes77@gmail.com>
2026-05-23 16:01:47 -0300

    feat(GetOrderUseCase): creating getOrderUseCase

---
+++ backend/src/main/java/com/ifsp/edu/sanca_dinner/application/order/use_cases/GetOrderUseCase.java
@@ -0,0 +1,20 @@
+package com.ifsp.edu.sanca_dinner.application.order.use_cases;
+
+import com.ifsp.edu.sanca_dinner.application.order.mapper.OrderMapper;
+import com.ifsp.edu.sanca_dinner.controller.order.response.OrderResponse;
+import com.ifsp.edu.sanca_dinner.domain.service.order.OrderService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@AllArgsConstructor
+public class GetOrderUseCase {
+
+    private OrderService orderService;
+    private OrderMapper orderMapper;
+
+    public OrderResponse execute(Integer orderId){
+        var order = orderService.findOrderById(orderId);
+        return orderMapper.orderToResponse(order);
+    }
+}
