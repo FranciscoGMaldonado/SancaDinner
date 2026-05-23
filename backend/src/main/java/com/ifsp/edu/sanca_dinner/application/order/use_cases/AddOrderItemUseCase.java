2e834bfc7a06232fb82e9dbd3f4d079ba32913e5
abelBaes <abel.baes77@gmail.com>
2026-05-23 16:20:51 -0300

    feat(CreateOrderItemRequest): creating request

---
+++ backend/src/main/java/com/ifsp/edu/sanca_dinner/application/order/use_cases/AddOrderItemUseCase.java
@@ -0,0 +1,17 @@
+package com.ifsp.edu.sanca_dinner.application.order.use_cases;
+
+import com.ifsp.edu.sanca_dinner.application.order.mapper.OrderMapper;
+import com.ifsp.edu.sanca_dinner.controller.order.response.OrderResponse;
+import com.ifsp.edu.sanca_dinner.domain.service.order.OrderService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@AllArgsConstructor
+public class AddOrderItemUseCase {
+
+    private OrderService orderService;
+    private OrderMapper orderMapper;
+
+    public OrderResponse execute()
+}
