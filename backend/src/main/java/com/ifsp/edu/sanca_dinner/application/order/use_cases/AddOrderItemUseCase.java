02deff8f5e951dbf62a31edf605c2fe1c00fbbbc
abelBaes <abel.baes77@gmail.com>
2026-05-23 16:32:11 -0300

    feat(AddOrderItem): creating use case

--- backend/src/main/java/com/ifsp/edu/sanca_dinner/application/order/use_cases/AddOrderItemUseCase.java
+++ backend/src/main/java/com/ifsp/edu/sanca_dinner/application/order/use_cases/AddOrderItemUseCase.java
@@ -1,8 +1,10 @@
 package com.ifsp.edu.sanca_dinner.application.order.use_cases;
 
 import com.ifsp.edu.sanca_dinner.application.order.mapper.OrderMapper;
+import com.ifsp.edu.sanca_dinner.controller.order.request.CreateOrderItemRequest;
 import com.ifsp.edu.sanca_dinner.controller.order.response.OrderResponse;
 import com.ifsp.edu.sanca_dinner.domain.service.order.OrderService;
+import com.ifsp.edu.sanca_dinner.domain.service.product.ProductService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -11,7 +13,11 @@ import org.springframework.stereotype.Component;
 public class AddOrderItemUseCase {
 
     private OrderService orderService;
+    private ProductService productService;
     private OrderMapper orderMapper;
 
-    public OrderResponse execute()
+    public OrderResponse execute(CreateOrderItemRequest request){
+        var order = orderService.addOrderItem(request.orderId(), request.productId(), request.specification(), productService.findProductById(request.productId()).getPrice());
+        return orderMapper.orderToResponse(order);
+    }
 }
