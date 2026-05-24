6962111a3b7f5e6dbae9510fda605af5860ef504
abelBaes <abel.baes77@gmail.com>
2026-05-24 15:25:08 -0300

    feat(OrderController): adding post mapping methods

--- backend/src/main/java/com/ifsp/edu/sanca_dinner/controller/order/OrderController.java
+++ backend/src/main/java/com/ifsp/edu/sanca_dinner/controller/order/OrderController.java
@@ -1,7 +1,16 @@
 package com.ifsp.edu.sanca_dinner.controller.order;
 
 import com.ifsp.edu.sanca_dinner.application.order.use_cases.*;
+import com.ifsp.edu.sanca_dinner.controller.order.request.CloseOrderRequest;
+import com.ifsp.edu.sanca_dinner.controller.order.request.CreateOrderItemRequest;
+import com.ifsp.edu.sanca_dinner.controller.order.request.CreateOrderRequest;
+import com.ifsp.edu.sanca_dinner.controller.order.request.ProgressOrderItemRequest;
+import com.ifsp.edu.sanca_dinner.controller.order.response.OrderResponse;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,12 +20,32 @@ import org.springframework.web.bind.annotation.RestController;
 public class OrderController {
 
     private AddOrderItemUseCase addOrderItemUseCase;
-    private CancelOrderItemUseCase cancelOrderItemUseCase;
-    private ChangeOrderItemUseCase changeOrderItemUseCase;
+    private CancelOrderItemUseCase cancelOrderItemUseCase; //Delete (/order_items)
+    private ChangeOrderItemUseCase changeOrderItemUseCase; //Put
     private CloseOrderUseCase closeOrderUseCase;
     private CreateOrderUseCase createOrderUseCase;
-    private GetAllActiveOrdersUseCase getAllActiveOrdersUseCase;
-    private GetAllOrdersUseCase getAllOrdersUseCase;
-    private GetOrderUseCase getOrderUseCase;
+    private GetAllActiveOrdersUseCase getAllActiveOrdersUseCase; //Get (/active
+    private GetAllOrdersUseCase getAllOrdersUseCase; //Get
+    private GetOrderUseCase getOrderUseCase; //Get (/{order_id}
     private ProgressOrderItemUseCase progressOrderItemUseCase;
+
+    @PostMapping
+    public ResponseEntity<OrderResponse> createOrder(@RequestBody CreateOrderRequest request){
+        return ResponseEntity.status(HttpStatus.CREATED).body(createOrderUseCase.execute(request));
+    }
+
+    @PostMapping("/order_items")
+    public ResponseEntity<OrderResponse> addOrderItem(@RequestBody CreateOrderItemRequest request){
+        return ResponseEntity.status(HttpStatus.CREATED).body(addOrderItemUseCase.execute(request));
+    }
+
+    @PostMapping("/close")
+    public ResponseEntity<OrderResponse> closeOrder(@RequestBody CloseOrderRequest request){
+        return ResponseEntity.status(HttpStatus.CREATED).body(closeOrderUseCase.execute(request));
+    }
+
+    @PostMapping("/progress")
+    public ResponseEntity<OrderResponse> closeOrder(@RequestBody ProgressOrderItemRequest request){
+        return ResponseEntity.status(HttpStatus.CREATED).body(progressOrderItemUseCase.execute(request));
+    }
 }
