352bff29e87299d81a16bac7d5e2a3b6bb7b350b
FranciscoGMaldonado <francisco.guaturamaldonado83@gmail.com>
2026-05-24 20:28:51 -0300

    feat: mock for all users

--- backend/src/main/resources/db/migration/V2__admin_user.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO users(email, name, password, role)
-VALUES ('admin@gmail.com', 'Admin Sanca Dinner', '$2a$12$9z0mIpTQqQNzB143WZMmV.tEOZ3QeuitbXRTfhA73ndgMsc/cD5EK', 'ADMIN');
---
+++ backend/src/main/resources/db/migration/V2__users.sql
@@ -0,0 +1,5 @@
+INSERT INTO users(email, name, password, role)
+VALUES
+('admin@gmail.com', 'Admin Sanca Dinner', '$2a$12$9z0mIpTQqQNzB143WZMmV.tEOZ3QeuitbXRTfhA73ndgMsc/cD5EK', 'ADMIN'),
+('atendimento@gmail.com', 'Atendimento Sanca Dinner', '$2a$12$ru./NjVw2ACy27cRrJ/Dgur3LNElQsYl6OPxsmyNNV7bbUasvjwpu', 'SERVICE'),
+('cozinha@gmail.com', 'Cozinha Sanca Dinner', '$2a$12$x8I4ItROT7hD8b.3e44saetGiTWTD/Fh5iHKbs1ucoiUml8SIAVGO', 'KITCHEN');
